bca66cae99a338445215f75fd0362aa4d7b64c23
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-27 22:42:48 -0300

    Refatora showData

--- js/script.js
+++ js/script.js
@@ -2,26 +2,23 @@ const cep = document.querySelector('#cep');
 
 const showData = (result) => {
     for (const campo in result) {
-        console.log(campo, ": ", result[campo]);
-        if(document.querySelector("#"+campo)) {
-            // console.log(campo);
-            document.querySelector("#"+campo).value = result[campo];
-        }
+        const exists = document.querySelector("#"+campo);
+        if(exists) exists.value = result[campo];
     }
 }
 
 cep.addEventListener('blur', (e) => {
 
     let search = cep.value.replace("-", "");
-
     const options = {
         method: 'GET',
         mode: 'cors',
         cache: 'default',
     }
-
     fetch(`https://viacep.com.br/ws/${cep.value}/json/`, options)
     .then( response => response.json() )
         .then( data => showData(data))
-    .catch()
+    .catch(error => {
+        alert(error);
+    });
 });
